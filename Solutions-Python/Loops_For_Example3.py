9cf4ce01ca39f19bafcf5c56844f0aa94f894307
dpjudge <dpj10@cam.ac.uk>
2018-07-19 15:22:09 +0100

    Thur 1 .... or maybe 2?
     Changes to be committed:
    	new file:   HTML/Flip_Day0.html
    	modified:   HTML/Python_Bits.html
    	modified:   Solutions-Python/Loops_For_Example3.py
    	deleted:    Solutions-Python/Loops_For_Example4.py#
    	new file:   Solutions-Python/Loops_For_Example5.py
    	deleted:    Solutions-Python/cat.py
    	deleted:    Solutions-Python/ddd

--- Solutions-Python/Loops_For_Example3.py
+++ Solutions-Python/Loops_For_Example3.py
@@ -1,14 +1,16 @@
 Sequence_file = open("homeobox_human.fasta"); # Open the target file for reading 
 
-Sequence_Count = 0; # Inintial the Annotation Line Counter variable 
-AA_Count = 0;       # Inintial the Amino Acid Counter variable 
+Sequence_Count = 0; # Initialise the Annotation Line Counter variable 
+AA_Count = 0;       # Initialise the Amino Acid Counter variable 
 
 for Line in Sequence_file:   # For each Line of Sequence_file 
     if Line[0] == ">":       # If this is an Annotation Line, 
         print Line;          # Print the Line, 
-        Sequence_Count += 1; # Increment the Sequence Couunter. 
+        Sequence_Count += 1; # Increment the Sequence Counter. 
         continue;            # move to the top of the Loop for the next Line 
-    AA_Count += len(Line);   # Increment the Sequence Couunter. 
+    AA_Count += len(Line);   # Increment the Sequence Counter. 
+
+Sequence_file.close(); # Close the file of sequences 
 
 # Print the Sequence & Amino Acid Counts 
 print "There are " + str(Sequence_Count) + " Protein Sequences in the file"
